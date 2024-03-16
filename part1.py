1b9a5b028d392e36100b4fe402e4b2b7f51311c0
Teja2121 <56876637+Teja2121@users.noreply.github.com>
2024-03-16 12:29:36 -0400

    part1.py

--- part1.py
+++ part1.py
@@ -35,7 +35,7 @@ def fit_kmeans(data, n_clusters):
     scalar = StandardScaler()
     X_scalar = scalar.fit_transform(X)
     #k-means
-    kmeans = cluster.KMeans(n_clusters = n_clusters, init = 'random', random_state=42)
+    kmeans = cluster.KMeans(n_clusters = n_clusters, init = 'random', random_state = 80, n_init = 10)
     kmeans.fit(X_scalar)
     # label prediction
     labels = kmeans.predict(X_scalar)
@@ -72,11 +72,11 @@ def compute():
     # Dictionary of 5 datasets. e.g., dct["nc"] = [data, labels]
     # 'nc', 'nm', 'bvv', 'add', 'b'. keys: 'nc', 'nm', 'bvv', 'add', 'b' (abbreviated datasets)
     dct = answers["1A: datasets"] = {}
-    dct["nc"] = [noisy_circles, 'nc']
-    dct["nm"] = [noisy_moons, 'nm']
-    dct["bvv"] = [blobs_varied, 'bvv']
-    dct["add"] = [anisotrop, 'add']
-    dct["b"] = [blobs_normal, 'b']
+    dct["nc"] = [noisy_circles, 'Noisy circles']
+    dct["nm"] = [noisy_moons, 'Noisy moons']
+    dct["bvv"] = [blobs_varied, 'Blobs with varied variance']
+    dct["add"] = [anisotrop, 'Anisotropicly distributed data ']
+    dct["b"] = [blobs_normal, 'Blobs']
     
 
     """
@@ -93,14 +93,38 @@ def compute():
     Create a pdf of the plots and return in your report. 
     """
 
+    datasets_list = [noisy_circles, noisy_moons, blobs_varied, anisotrop, blobs_normal]
+    dataset_names = ['Noisy Circles', 'Noisy Moons', 'Blobs with Varied Variances', 'Anisotropic', 'Blobs Normal']
+
+    # Defining the total number of clusters with the n_clusters_list 
+    n_clusters_list = [2, 3, 5, 10]
+
+    # Create a figure with subplots in a 4x5 grid
+    fig, axis = plt.subplots(nrows = 4, ncols = 5, figsize = (22, 16))
+
+
+    for i, n_clusters in enumerate(n_clusters_list):
+        for j, dataset in enumerate(datasets_list):
+            labels = fit_kmeans(dataset, n_clusters)
+            axis[i, j].scatter(dataset[0][:, 0], dataset[0][:, 1], c = labels, s = 8, cmap = 'viridis')
+            axis[i, j].set_xticks([])
+            axis[i, j].set_yticks([])
+            if i == 0:
+                axis[i, j].set_title(dataset_names[j])
+            if j == 0:
+                axis[i, j].set_ylabel(f"k={n_clusters}")
+
+    fig.suptitle('Part 1 - Plots', fontsize = 15) 
+    plt.show()
+
     # dct value: return a dictionary of one or more abbreviated dataset names (zero or more elements) 
     # and associated k-values with correct clusters.  key abbreviations: 'nc', 'nm', 'bvv', 'add', 'b'. 
     # The values are the list of k for which there is success. Only return datasets where the list of cluster size k is non-empty.
-    dct = answers["1C: cluster successes"] = {"xy": [3,4], "zx": [2]} 
+    dct = answers["1C: cluster successes"] = {"nm": [10], "bvv": [2,10], "add" : [2, 10], "b" : [2,3,5,10]} 
 
     # dct value: return a list of 0 or more dataset abbreviations (list has zero or more elements, 
     # which are abbreviated dataset names as strings)
-    dct = answers["1C: cluster failures"] = ["xy"]
+    dct = answers["1C: cluster failures"] = ["nc"]
 
     """
     D. Repeat 1.C a few times and comment on which (if any) datasets seem to be sensitive to the choice of initialization for the k=2,3 cases. You do not need to add the additional plots to your report.
@@ -111,7 +135,7 @@ def compute():
     # dct value: list of dataset abbreviations
     # Look at your plots, and return your answers.
     # The plot is part of your report, a pdf file name "report.pdf", in your repository.
-    dct = answers["1D: datasets sensitive to initialization"] = [""]
+    dct = answers["1D: datasets sensitive to initialization"] = ["nm"]
 
     return answers
 
